1ddfd72fbe101dc6b8ab7d1b20fb81e13cd11d92
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-02-17 16:18:07 +0000

    line 25 too long

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+	int last_dig;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_dig = n % 10;
+
+	if (n > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_dig);
+	else if (n == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last_dig);
+	else
+		printf("Last digit of %d is %d and is less than 6 and is not 0\n", n, last_dig);
+	return (0);
