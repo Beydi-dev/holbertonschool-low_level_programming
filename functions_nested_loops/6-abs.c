c0d7460d4d0cfd012e1af243e9d0cc6d242fd571
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-02-19 13:27:11 +0000

    First Try

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value of c
+ * @c : the character
+ * Return: absolute value
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+		return (-c);
+	else
+		return (c);
+}
+
