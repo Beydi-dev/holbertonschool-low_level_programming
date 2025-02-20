3d40f7d7a640630c07babb1d754034d99ab6b445
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-02-20 21:45:14 +0000

    98

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - addition between two integers
+ * @a et @b: The integers
+ *
+ * Return: Value of the addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
