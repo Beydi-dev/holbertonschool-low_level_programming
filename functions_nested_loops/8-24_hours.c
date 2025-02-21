a6b31e742e9b210c36395b323228e4189c3dfd13
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-02-21 12:58:46 +0000

    first commit

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of a day starting from 00:00 to 23:59.
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hours;
+	int minutes;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar(hours / 10 + '0');
+			_putchar(hours % 10 + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(minutes % 10 + '0');
+			_putchar ('\n');
+		}
+	}
+}
