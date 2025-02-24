8ef3b2898c4e7d206f76aadc5d17199e67f0d3f2
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-02-24 13:06:06 +0000

    First Try

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: The first int
+ * @b: the second int
+ *
+ * Return: Value of the last digit.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
