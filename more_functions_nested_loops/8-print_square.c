2896688719823e58b5d4ff928b7adcb472f58c06
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-02-25 10:10:48 +0000

    First Try

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square
+ * @size: format of the squae
+ *
+ * Return: Value of the last digit.
+ */
+
+void print_square(int size)
+{
+	int idx1, idx2;
+
+	for (idx1 = 0; idx1 < size; idx1++)
+	{
+		for (idx2 = 0; idx2 < size; idx2++)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}
+}
