b9ba8d9df6e51387830d457c294de3429ee79a18
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-02-25 09:43:58 +0000

    errors

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Prints \ depending on n
+ * @n: the parameter
+ *
+ * Return: \
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0; /* i pour itÃrer */
+	int c = 0; /* c pour compter les espaces nÃcessaires*/
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			c = 0;
+			while (c < i)
+			{
+				_putchar(' ');
+				c++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
