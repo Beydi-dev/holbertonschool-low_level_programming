d5dbcf971141cab5fea63fd3b75de2d49f86e071
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-02-28 15:13:47 +0000

    First Try

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * _atoi - prints digits sign
+ * @str: The number in question.
+ *
+ * Return: result * sign
+ */
+
+int _atoi(const char *str)
+{
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+	{
+		i++;
+	}
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+		{
+			sign = -sign;
+		}
+		i++;
+	}
+	while ((str[i] >= '0') && (str[i] <= '9'))
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
