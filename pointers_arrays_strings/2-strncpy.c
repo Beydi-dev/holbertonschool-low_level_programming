cce7f57365872c293c0dba800d13678322f20c24
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-03-02 14:29:21 +0000

    First Try

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strncpy - Copies the string pointed by src to dest
+ * @src: The source
+ *@dest: The destination
+ *@n: The counter
+ * Return: Destination
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
