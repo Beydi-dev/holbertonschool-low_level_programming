bb4175921fbc92e32c89321e6543431daf310dea
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-03-02 16:42:03 +0000

    First Try

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - Prints the last digit of a number.
+ * @a: The array of integers
+ * @n: The counter
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+	
+	while (i < n / 2)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+		i++;
+	}
+}
+
+void print_array(int *a, int n)
+{
+    int i;
+
+    i = 0;
+    while (i < n)
+    {
+        if (i != 0)
+        {
+            printf(", ");
+        }
+        printf("%d", a[i]);
+        i++;
+    }
+    printf("\n");
+}
