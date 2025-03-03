78a105ac149de230ba952c5fc244d2926983a866
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-03-03 15:49:55 +0000

    Retry

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -14,9 +14,13 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		j = 0;
 		while (accept[j] != '\0' && s[i] != accept[j])
+		{
 			j++;
-		if (accept[j] == '\0')
+			if (accept[j] == '\0')
+			{
 			return (i);
+			}
+		}
 		i++;
 	}
 	return (i);
