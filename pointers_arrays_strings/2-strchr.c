6f785e7d10db9d8c83298dcb945ab6812452648a
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-03-03 13:53:13 +0000

    First Try

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * _strchr - locates a character in a string
+ * @s: The string
+ * @c:the character to be found
+ * Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
