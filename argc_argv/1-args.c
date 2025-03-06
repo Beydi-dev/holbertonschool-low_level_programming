a9ff9463478ed978fdf90ba371ed769cf964c4d3
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-03-06 14:27:19 +0000

    First Try

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: void
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
