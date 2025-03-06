3274ccfc1f7e0a36a212ed39725c8981d8f72e32
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-03-06 14:04:18 +0000

    First Try

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: void
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return(0);
+}
