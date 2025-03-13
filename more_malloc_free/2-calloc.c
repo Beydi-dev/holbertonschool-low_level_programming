a5d7fd74931f84a039a1d83a9072075b233f5a40
Beyy93 <beydi.coulibaly2408@gmail.com>
2025-03-13 14:53:09 +0100

    Retry

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,32 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * _calloc - allocates memory for an array 
+ * _calloc - allocates memory for an array
  * @nmemb: elements of the array
  * @size: Size of the array
  * Return: void*
  */
 
- void *_calloc(unsigned int nmemb, unsigned int size)
- {
-    char *ptr;
-    int i;
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
 
-    if (nmemb == 0 || size == 0)
-    {
-        return (NULL);
-    }
-    ptr = malloc(nmemb * size);
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * size);
 
-    if (ptr == NULL)
-    {
-        exit(1);
-    }
-    for (i = 0; i < nmemb * size; i++)
-    {
-        ptr[i] = 0;
-    }
-    return (ptr);
+	if (ptr == NULL)
+	{
+		exit(1);
+	}
+	for (i = 0; i < nmemb * size; i++)
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
 }
