401d6f45efad0ae7b40f5f58d243b34be3ba91f8
Beyy93 <beydi.coulibaly2408@gmail.com>
2025-03-17 15:08:19 +0100

    Retry

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stddef.h>
+
 /**
  * print_name - Print a name from a function pointer
  * @name: char string
@@ -8,8 +8,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-	{
 		f(name);
-	}
 }
