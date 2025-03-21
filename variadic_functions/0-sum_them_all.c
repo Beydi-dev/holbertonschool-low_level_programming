cbe1963e3cd0ffd3481e5c3aba978b43d23eaea7
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-03-21 16:57:59 +0100

    Retry

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,14 +2,14 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - Returns the sum of all its parameters
- * @n: The number of arguments to be summed
- * 
- * Description: This function takes a variable number of arguments
- * and returns their sum. If n is 0, the function returns 0.
- * 
- * Return: The sum of the arguments, or 0 if n == 0
- */
+* sum_them_all - Returns the sum of all its parameters
+* @n: The number of arguments to be summed
+*
+* Description: This function takes a variable number of arguments
+* and returns their sum. If n is 0, the function returns 0.
+*
+* Return: The sum of the arguments, or 0 if n == 0
+*/
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
@@ -21,7 +21,7 @@ int sum_them_all(const unsigned int n, ...)
 	{
 		return (0);
 	}
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
