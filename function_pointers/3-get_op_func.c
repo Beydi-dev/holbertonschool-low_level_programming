f6ba1d7ccc0fdfc3ede470f5c9329b173fefa0ef
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-03-21 17:44:32 +0100

    Retry

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -25,7 +25,7 @@ int (*get_op_func(char *s))(int, int)
 /*iterating through the array with integer i to find a match*/
 while (ops[i].op)
 {
-	if (*s == *ops[i].op)
+	if (strcmp(s, ops[i].op) == 0)
 	{
 		return (ops[i].f);
 	}
