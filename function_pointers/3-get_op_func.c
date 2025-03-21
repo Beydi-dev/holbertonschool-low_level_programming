be8a9df72d3b2dc67a85f78f0a3dbf05b242ae7b
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-03-21 18:08:54 +0100

    Retry

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,6 +1,6 @@
 #include "3-calc.h"
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 /**
  * get_op_func - Retrieves the function corresponding to an operator
@@ -20,14 +20,13 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 
-	while (ops[i].f != NULL)
+	while (ops[i].op)
 	{
 		if (*s == *(ops[i].op) && s[1] == '\0')
-		{
 			return (ops[i].f);
-			i++;
-		}
-		printf("Error\n");
-		exit(99);
+		i++;
 	}
+
+	printf("Error\n");
+	exit(99);
 }
