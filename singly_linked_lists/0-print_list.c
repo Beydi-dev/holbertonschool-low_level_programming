fd85cb1bb0e639078051dee579b91d4f88d1b749
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-04-01 12:58:28 +0200

    adding function description

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,18 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_list - Prints all the elements of a linked list of type list_t.
+ * @h: Pointer to the first node of the list.
+ *
+ * Return: The total number of nodes in the list.
+ *
+ * This function loops through each node of the linked list.
+ * For each node, it prints:
+ * - "[0] (nil)" if the string is NULL.
+ * - Otherwise, it prints the length of the string and the string itself.
+ * It returns the total number of nodes traversed.
+ */
 size_t print_list(const list_t *h)
 {
 	unsigned int i = 0;
