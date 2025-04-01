cbfc086b0f6965468125ee71270b305a90585976
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-04-01 17:30:32 +0200

    First Try

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: Double pointer to the head of the list.
+ * @str: String to be duplicated and stored in the new node.
+ *
+ * Return: Address of the new element, or NULL if it failed.
+ *
+ * This function allocates memory for a new node,
+ * duplicates the given string into the node,
+ * sets its length, and links it at the beginning of the list.
+ * If memory allocation or string duplication fails, it returns NULL.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+/* first we creating the new node and checking if the malloc fails */
+	list_t *newNode = malloc(sizeof(list_t));
+
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	newNode->str = strdup(str);
+
+	if (newNode->str == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+	newNode->len = strlen(str);
+	newNode->next = *head;
+	*head = newNode;
+	return (newNode);
+}
