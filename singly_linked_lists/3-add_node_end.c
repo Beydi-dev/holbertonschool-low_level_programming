1c432993b1442e87580fa44a19b5abebf9d4ebe1
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-04-02 18:34:56 +0200

    adding while condition for str length

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -37,6 +37,10 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new_node;
 		return (new_node);
 	}
+	while (str[i] != '\0')
+	{
+		i++;
+	}
 
 	ptr = *head;
 	while (ptr->next != NULL)
