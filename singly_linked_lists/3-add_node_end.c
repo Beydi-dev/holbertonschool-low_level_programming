1f25a269c79519139a75c2d0101f084382750b0b
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-04-02 18:44:30 +0200

    Retry

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -15,11 +15,6 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *ptr;
 	unsigned int i = 0;
 
-	if (head == NULL || str == NULL)
-	{
-		return (NULL);
-	}
-
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
