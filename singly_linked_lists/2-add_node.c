c124fae35770b880c6a20b9c4e37ab1ac0b68279
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-04-02 12:14:06 +0200

    adding verification of **head

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,27 +3,27 @@
 #include <stdlib.h>
 
 /**
- * add_node - Adds a new node at the beginning of a list_t list.
- * @head: Double pointer to the head of the list.
- * @str: String to be duplicated and stored in the new node.
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: double pointer to the head of the list
+ * @str: string to duplicate and put in the new node
  *
- * Return: Address of the new element, or NULL if it failed.
- *
- * This function allocates memory for a new node,
- * duplicates the given string into the node,
- * sets its length, and links it at the beginning of the list.
- * If memory allocation or string duplication fails, it returns NULL.
+ * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-/* first we create the new node and checking if the malloc fails */
+	if (head == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+
+	/* Allocate memory for the new node */
 	list_t *newNode = malloc(sizeof(list_t));
 
 	if (newNode == NULL)
 	{
 		return (NULL);
 	}
-	/* then duplicating str and checking if strdup fails*/
+	/* Duplicate the string */
 	newNode->str = strdup(str);
 
 	if (newNode->str == NULL)
@@ -31,7 +31,7 @@ list_t *add_node(list_t **head, const char *str)
 		free(newNode);
 		return (NULL);
 	}
-	/* assigning newNode to the head and head to newNode*/
+	/* Set node data and link it to the list */
 	newNode->len = strlen(str);
 	newNode->next = *head;
 	*head = newNode;
