ecf5bdef13e8f45244f8be84fa5d2dee926bcd3e
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-04-02 13:19:16 +0200

    Retry

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,17 +11,17 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *ptr;
+	list_t *new_node;
+	list_t *ptr;
 
-	ptr = head;
-	new_node = (list_t *)malloc(sizeof(list_t));
-
-	if (new_node == NULL)
-	{
+	if (head == NULL || str == NULL)
 		return (NULL);
-	}
-	new_node->str = strdup(str);
 
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
@@ -31,10 +31,17 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = strlen(str);
 	new_node->next = NULL;
 
-	while (ptr->next != NULL)
+	if (*head == NULL)
 	{
-		ptr = ptr->next;
+		*head = new_node;
+		return (new_node);
 	}
+
+	ptr = *head;
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+
 	ptr->next = new_node;
+
 	return (new_node);
 }
