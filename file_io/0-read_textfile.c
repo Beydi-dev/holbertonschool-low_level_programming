5532996e3e2bc83430c45c78dc21142cda5ab3de
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-04-06 17:39:02 +0200

    0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -19,19 +19,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	buffer = malloc(letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
 	}
 
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
