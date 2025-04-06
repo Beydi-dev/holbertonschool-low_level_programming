cf99de86bc6ae1084e7f3d003f80ef22f418ab9e
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-04-06 19:28:41 +0200

    Retry

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -36,8 +36,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	/* Open the file in append mode */
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
+	{
 		return (-1);
-
+	}
 	if (text_content != NULL)
 	{
 		bytes_write = write(fd, text_content, _strlen(text_content));
